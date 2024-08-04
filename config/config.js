1eb12ff20a646790b78615e71ca31aded00f292d
Remigious07 <derrickremigious@gmail.com>
2024-08-04 20:40:12 +0530

    A portfolio website template that helps you showcase your work, projects and skills as a software developer or freelancer.

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ import { faAppStore, faGithub, faGooglePlay } from '@fortawesome/free-brands-svg
 import { } from '@fortawesome/free-solid-svg-icons';
 
 export const navigation = {
-  name: "Hashir",
+  name: "Derrick Remigious",
   links: [
     {
       title: "About",
@@ -22,15 +22,12 @@ export const navigation = {
       title: "Links",
       link: "/links",
     },
-    {
-      title: "Blog",
-      link: "https://medium.com/@hashirshoaeb",
-    }
+   
   ],
 }
 export const intro = {
-  title: "Hey, I'm Hashir",
-  description: "A Computer Engineer creating mobile apps and static websites.",
+  title: "Hey, I'm Derrick Remigious",
+  description: "IT engineering fresher with expertise in data science, machine learning, and web development.",
   image: profile.src,
   buttons: [
     {
@@ -40,7 +37,7 @@ export const intro = {
     },
     {
       title: "Resume",
-      link: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+      link: "https://drive.google.com/file/d/1UZZriidAUoa39xFradEtkl3lBau7_hic/view?usp=drive_link",
       isPrimary: false,
     },
   ],
@@ -49,25 +46,32 @@ export const intro = {
 export const about = {
   title: "Who I am",
   description: [
-    "I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering. I kick started my professional career as a mobile application developer working remotely for a UK based startup — Lessgo.",
-    "The tech industry is ever-evolving and I love being able to grow with it, while continually solidifying the fundamentals. I opensource my code from a place of empathy - for future developers, teammates, users, and with accessibility in mind. I also love joining communities, helping and mentoring new developers, and supporting organizations to promote diversity in tech. I’m sharing my programming journey on instagram, helping people who are just getting into the space of programming.",
-    "When I’m not programming, I focus on my hobbies which are: diary writing, doodling, meeting people and growing my network.",
+    "Hello! I’m currently pursuing my B.Tech in Information Technology at Prathyusha Engineering College, with a planned graduation in 2025. I have maintained a CGPA of 8.2, reflecting my dedication and commitment to my studies. My education has provided me with a solid foundation in IT principles and practices, preparing me for a dynamic career in technology.",
+    "I have honed my skills in web development, data analytics, UI/UX design, and core IT concepts. My expertise includes creating dynamic and responsive web applications, analyzing and interpreting complex data sets to generate actionable insights, and designing user-friendly interfaces that enhance user experience. This diverse skill set equips me to tackle a variety of technical challenges and contribute effectively to any project.",
+    "I am passionate about leveraging my knowledge to solve real-world problems and make a meaningful impact. Whether it’s developing innovative web solutions or uncovering insights through data analysis, I am driven by a desire to contribute to impactful projects. I am excited to connect with like-minded professionals and explore opportunities to collaborate and create something amazing together.",
   ],
 }
 
 export const work = {
   title: "What I do",
   cards: [
+   
     {
-      title: "Mobile App Development",
-      description: "I create pixel perfect iOS and Andriod apps using Flutter.",
+      title: "Web Development",
+      description: "A web developer crafts digital experiences, blending creativity and technical prowess to build captivating and intuitive websites that engage users effectively.",
       icons: null,
     },
     {
-      title: "Web Development",
-      description: "I create responsive static websites using Reactjs.",
+      title: "Data Analytics",
+      description: "A data analyst translates complex data into actionable insights, driving informed decisions and strategic outcomes through meticulous analysis and visualization techniques.",
       icons: null,
-    }
+    },
+    {
+      title: "Android App Development",
+      description: "An Android app developer designs and builds applications for mobile devices running the Android operating system, focusing on creating intuitive user interfaces and robust functionality to enhance user experiences.",
+      icons: null,
+    },
+
   ],
 }
 
@@ -75,31 +79,26 @@ export const projects = {
   title: "Projects",
   cards: [
     {
-      title: "StarBook",
-      description: "A digital diary and mood tracking app that helps you in keeping track of your mood and productivity throughout the month/year.",
+      title: "E-Commerce Website",
+      description: "Successfully led the end-to-end development of a responsive E- Commerce website, implementing secure payment gateways, inventory management, and analytics tools, resulting in increased user engagement and business growth.",
       icons: [
-        {
-          icon: faAppStore,
-          link: "https://apps.apple.com/us/app/starbook-journal-ai-diary/id1552418289",
-        },
+
         {
           icon: faGithub,
-          link: "https://github.com/hashirshoaeb/star_book",
+          link: "",
         },
       ]
     },
     {
-      title: "QuranTalk",
-      description: "An emotional well being and mental health app. The app helps people to navigate their emotions in the light of the Quran.",
+      title: "Online Bookstore Management System",
+      description: "Developed an Online Bookstore Management System in Python, featuring user authentication, inventory management, and order processing, highlighting proficiency in backend development and e-commerce functionality.",
       icons: [
+
         {
-          icon: faAppStore,
-          link: "https://apps.apple.com/us/app/qurantalk/id1563425149",
+          icon: faGithub,
+          link: "",
         },
-        {
-          icon: faGooglePlay,
-          link: "https://play.google.com/store/apps/details?id=com.ayahemotion.quran_talk",
-        },
+
       ]
     },
     {
@@ -108,7 +107,7 @@ export const projects = {
       icons: [
         {
           icon: faGithub,
-          link: "https://github.com/hashirshoaeb/portfolio",
+          link: "",
         },
       ]
     },
@@ -117,53 +116,49 @@ export const projects = {
 
 export const contact = {
   title: "Get in touch",
-  description: "Coffee Chat! Please do not hesitate to schedule a meeting. Alternatively, feel free to reach out directly by email at hashirshoaeb@gmail.com.",
+  description: "Coffee Chat! Please do not hesitate to ask questions, feel free to reach out directly by email at derrickremigious@gmail.com.",
   buttons: [
     {
       title: "Email Me",
-      link: "mailto:hashirshoaeb@gmail.com",
+      link: "mailto:derrickremigious@gmail.com",
       isPrimary: true,
     },
-    {
-      title: "Schedule Meeting",
-      link: "https://topmate.io/hashirshoaeb",
-      isPrimary: false,
-    },
+    
   ]
 }
 
 // SEARCH ENGINE 
 export const SEO = {
   // 50 - 60 char  
-  title: "Hashir Shoaib | Computer Engineer | Flutter | Reactjs developer",
-  description: "I create mobile apps and static websites. I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering.",
+  title: "Derrick Remigious | IT Engineer | Web Developer | Data Analyst",
+  description: "Enthusiastic entry-level IT professional with a strong foundation in web development and a fervent passion for data science. Possessing hands-on expertise in machine learning, I am eager to contribute to innovative projects, demonstrate adaptability in dynamic environments, and actively contribute to the growth of a forward-thinking team.",
   image: profile.src,
 }
 
 export const links = {
   image: profile.src,
-  title: "@hashirshoaeb",
-  description: "Computer Engineer | Flutter | Reactjs Developer",
+  title: "@derrickremigious",
+  description: "IT Engineer | Web Developer | Data Analyst",
   cards: [
     {
       title: "My website",
-      link: "https://hashirshoaeb.com/",
+      link: "",
     },
     {
-      title: "QuranTalk App",
-      link: "https://www.qurantalk.app/",
+      title: "E-Commerce Website",
+      link: "",
     },
     {
-      title: "StarBook App",
-      link: "https://starbook.dev/",
+      title: "Online Bookstore Management System",
+      link: "",
     },
     {
       title: "My GitHub",
-      link: "https://github.com/hashirshoaeb/",
+      link: "https://github.com/Remigious07",
     },
     {
       title: "My LinkedIn",
-      link: "https://www.linkedin.com/in/hashirshoaeb/",
+      link: "https://www.linkedin.com/in/derrickremigious/",
     },
   ]
 }
